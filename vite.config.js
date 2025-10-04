3a9229ac227ca7f92035e90567201bc261f77874
zafarzieng <zafarz786ai@gmail.com>
2025-10-04 23:51:57 +0500

    fix: Update build configuration to properly set output directory

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    }
+    outDir: '.output/static',
+    emptyOutDir: true,
   },
   server: {
     open: true
